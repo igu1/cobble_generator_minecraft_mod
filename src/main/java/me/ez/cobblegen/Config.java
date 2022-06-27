7010d350228751f6511d9557a33630d7182290a3
GAMER'S VALLEY <eesaard@gmail.com>
2022-06-27 21:26:34 +0530

    Develepment Restarted

---
+++ src/main/java/me/ez/cobblegen/Config.java
@@ -0,0 +1,4 @@
+package me.ez.cobblegen;
+
+public class Config{
+}
--- src/main/java/me/ez/cobblegen/Events.java
+++ src/main/java/me/ez/cobblegen/Events.java
@@ -2,6 +2,10 @@ package me.ez.cobblegen;
 
 import me.ez.cobblegen.Client.Screens.CobbleGenScreen;
 import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.layers.RenderLayer;
+import net.minecraftforge.client.event.RenderBlockOverlayEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -13,5 +17,6 @@ public class Events {
     public static void onClientSetup(FMLClientSetupEvent event)
     {
         MenuScreens.register(Init.COBBLE_GEN_CONTAINER_REGISTRY_OBJECT.get(), CobbleGenScreen::new);
+        ItemBlockRenderTypes.setRenderLayer(Init.COBBLE_GEN_BLOCK_REGISTRY_OBJECT.get(), RenderType.translucent());
     }
 }
