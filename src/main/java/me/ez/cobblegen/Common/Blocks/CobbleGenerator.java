5672576d9b1c113f4db2e5838d15e05f796e5634
igu1 <eesaard@gmail.com>
2022-07-08 15:11:40 +0530

    1.19 Voxel Set

--- src/main/java/me/ez/cobblegen/Common/Blocks/CobbleGenerator.java
+++ src/main/java/me/ez/cobblegen/Common/Blocks/CobbleGenerator.java
@@ -7,13 +7,21 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.BooleanOp;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.stream.Stream;
 
 public class CobbleGenerator extends AbstractBlockEntityBlock {
     public CobbleGenerator(Properties properties) {
@@ -23,6 +31,18 @@ public class CobbleGenerator extends AbstractBlockEntityBlock {
     @Override
     public @Nullable BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
         return new CobbleGeneratorBlockEntity(pos, state);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState p_60555_, BlockGetter p_60556_, BlockPos p_60557_, CollisionContext p_60558_) {
+        return Stream.of(
+                Block.box(0, 0, 0, 16, 6, 16),
+                Block.box(1, 6, 1, 2, 15, 14),
+                Block.box(0, 15, 0, 16, 16, 16),
+                Block.box(1, 6, 14, 14, 15, 15),
+                Block.box(14, 6, 2, 15, 15, 15),
+                Block.box(2, 6, 1, 15, 15, 2)
+        ).reduce((v1, v2) -> Shapes.join(v1, v2, BooleanOp.OR)).get();
     }
 
     @Override
